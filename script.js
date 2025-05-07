2b3b11c3369bd2ff8b95d7aadcf0d69695e7cc89
halima-gonah <halimagonah@gmail.com>
2025-05-07 19:22:17 +0300

    INitial commit

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 'use strict';
+const productData = [
+  {
+    name: 'Amber bloom perfume',
+    description: 'Amber, rose, and soft spices',
+    price: 340,
+    quantity: 5,
+    type: 'fragrance',
+  },
+];
